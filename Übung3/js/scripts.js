fd32f4a100e16f0b7c65d27b8b7cc7e269b77705
Nino Padrutt <nino.padrutt@hotmail.com>
2018-03-16 18:26:20 +0100

    complete weblab excercise 3

--- Übung3/js/scripts.js
+++ Übung3/js/scripts.js
@@ -1,39 +1,77 @@
 $(document).ready(function() {
 
-    var minutesStart = 0;
-    var secondsStart = 0;
-
-    var minutesCurrentValue = 0;
-    var secondsCurrentValue = 0;
+    var currentMinutes = $("#timeLabelMinutes").text();
+    var currentSeconds = $("#timeLabelSeconds").text();
 
     var myInterval;
+
+    $("#btnTimeUpLeft").click(timeUp);
+    $("#btnTimeUpRight").click(timeUp);
+
+    $("#btnTimeDownLeft").click(timeDown);
+    $("#btnTimeDownRight").click(timeDown);
 
     $("#btnStart").click(startTimer);
     $("#btnStop").click(stopTimer);
     $("#btnReset").click(resetTimer);
 
+    function timeUp() {
+        if(currentSeconds === 55){
+            currentSeconds = 00;
+            currentMinutes++;
+        }
+        else{
+            currentSeconds = parseInt(currentSeconds) + 5;
+        }
+
+        updateTimerUI();
+    }
+
+    function timeDown() {
+
+        if(currentMinutes === 0 && currentSeconds === 0){
+            alert("Timer kann nicht in den negativen Bereich gestellt werden");
+            return;
+        }
+
+        if(currentSeconds === 5){
+            currentSeconds = 0;
+            if(currentMinutes === 0){
+                return;
+            }
+            currentMinutes--;
+        }
+        else if(currentSeconds === 0 && currentMinutes !== 0) {
+            currentSeconds = 55
+        }
+        else{
+            currentSeconds = parseInt(currentSeconds) - 5;
+        }
+
+        updateTimerUI();
+    }
+
 
     function startTimer() {
-        minutesStart = $("#timeLabelMinutes").text();
-        secondsStart = $("#timeLabelSeconds").text();
-
-        minutesCurrentValue = $("#timeLabelMinutes").text();
-        secondsCurrentValue = $("#timeLabelSeconds").text();
-
         myInterval = setInterval(reduceTime, 1000)
     }
 
     function reduceTime(){
 
-        if(secondsCurrentValue == 0){
-            minutesCurrentValue --;
-            secondsCurrentValue = 59;
+        if(currentSeconds === 0){
+            currentMinutes --;
+            currentSeconds = 59;
         } else {
-            secondsCurrentValue--;
+            currentSeconds--;
         }
 
-        $("#timeLabelMinutes").text(minutesCurrentValue);
-        $("#timeLabelSeconds").text(secondsCurrentValue);
+        $("#timeLabelMinutes").text(currentMinutes);
+        $("#timeLabelSeconds").text(currentSeconds);
+
+        if(currentMinutes === 0 && currentSeconds === 0){
+            stopTimer();
+            alert("Timer fertig!");
+        }
     }
 
     function stopTimer() {
@@ -43,7 +81,12 @@ $(document).ready(function() {
     function resetTimer() {
         stopTimer();
 
-        $("#timeLabelMinutes").text(minutesStart);
-        $("#timeLabelSeconds").text(secondsStart);
+        $("#timeLabelMinutes").text(2);
+        $("#timeLabelSeconds").text(30);
+    }
+
+    function updateTimerUI(){
+        $("#timeLabelMinutes").text(currentMinutes);
+        $("#timeLabelSeconds").text(currentSeconds);
     }
 });
